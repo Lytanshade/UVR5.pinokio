93411544b8a1c7cf26625de718dd123cf5ccca45
Lytan <116470525+Lytanshade@users.noreply.github.com>
2024-04-28 02:51:31 +1200

    Update pinokio.js

--- pinokio.js
+++ pinokio.js
@@ -1,7 +1,7 @@
 const path = require('path')
 module.exports = {
   version: "1.5",
-  title: "UltimateVocalRemover_5",
+  title: "UltimateVocalRemover5",
   description: "UVR5 - The ultimate vocal remover application.  Ported from https://huggingface.co/spaces/Eddycrack864/UVR5 with added multiplatform GPU support. Orginal project: https://github.com/Anjok07/ultimatevocalremovergui   (12GB install.  GPU Conversion setting requires 6GB+ VRAM)",
   icon: "icon.png",
   menu: async (kernel) => {
