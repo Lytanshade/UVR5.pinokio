9a4a0d6a2914053e0d785d00d7e173540f1fb885
Lytan <116470525+Lytanshade@users.noreply.github.com>
2024-04-28 02:39:35 +1200

    Add files via upload

---
+++ pinokio.js
@@ -0,0 +1,64 @@
+const path = require('path')
+module.exports = {
+  version: "1.5",
+  title: "UltimateVocalRemover_5",
+  description: "UVR5 - The ultimate vocal remover application.  Ported from https://huggingface.co/spaces/Eddycrack864/UVR5 with added multiplatform GPU support. Orginal project: https://github.com/Anjok07/ultimatevocalremovergui   (12GB install.  GPU Conversion setting requires 6GB+ VRAM)",
+  icon: "icon.png",
+  menu: async (kernel) => {
+    let installing = await kernel.running(__dirname, "install.js")
+    let installed = await kernel.exists(__dirname, "app", "env")
+    let running = await kernel.running(__dirname, "start.js")
+    if (installing) {
+      return [{
+        icon: "fa-solid fa-plug",
+        text: "Installing",
+        href: "install.js",
+      }]
+    } else if (installed) {
+      if (running) {
+        let local = kernel.memory.local[path.resolve(__dirname, "start.js")]
+        if (local && local.url) {
+          return [{
+            icon: "fa-solid fa-rocket",
+            text: "Open Web UI",
+            href: local.url,
+          }, {
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start.js",
+          }]
+        } else {
+          return [{
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start.js",
+          }]
+        }
+      } else {
+        return [{
+          icon: "fa-solid fa-power-off",
+          text: "Start",
+          href: "start.js",
+        }, {
+          icon: "fa-solid fa-plug",
+          text: "Update",
+          href: "update.js",
+        }, {
+          icon: "fa-solid fa-plug",
+          text: "Install",
+          href: "install.js",
+        }, {
+          icon: "fa-regular fa-circle-xmark",
+          text: "Reset",
+          href: "reset.js",
+        }]
+      }
+    } else {
+      return [{
+        icon: "fa-solid fa-plug",
+        text: "Install",
+        href: "install.js",
+      }]
+    }
+  }
+}
